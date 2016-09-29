60e59a9658cf8471c436206ff35865694491e33a
Ivan <kiberaction@yandex.ru>
2016-09-30 01:21:29 +0300

    added send method

--- src/main/java/com/router/web/DataController.java
+++ src/main/java/com/router/web/DataController.java
@@ -1,11 +1,22 @@
 package com.router.web;
 
 import com.router.data.DeviceRepository;
+import com.router.data.SystemRepository;
 import com.router.data.vo.Device;
+import com.router.data.vo.System;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 /**
  * Created by asus-pc on 29.09.2016.
@@ -17,16 +28,29 @@ public class DataController {
     @Autowired
     private DeviceRepository deviceRepository;
 
-    @RequestMapping(value = "/send", method = RequestMethod.POST)
-    public ResponseEntity sendData(@RequestParam String id, @RequestBody String str) {
+    @Autowired
+    private SystemRepository systemRepository;
+
+    @RequestMapping(value = "/send", method = { RequestMethod.POST, RequestMethod.GET })
+    public ResponseEntity sendData(@RequestParam(required = true) String id, String str) {
         Device device = deviceRepository.findOne(id);
         if (device != null) {
-            String url = device.getDevice_url();
-            return new ResponseEntity(HttpStatus.OK);
-        } else {
-            return new ResponseEntity(HttpStatus.FORBIDDEN);
+            //рассылаем всем системам данные
+            OkHttpClient client = new OkHttpClient();
+            for (System s: systemRepository.findAll()) {
+                RequestBody body = RequestBody.create(null, str);
+                Request request = new Request.Builder()
+                            .url(s.getSystem_url())
+                            .post(body)
+                            .build();
+                try {
+                    Response response = client.newCall(request).execute();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
-
+        return new ResponseEntity(HttpStatus.OK);
     }
 
 }
