f66e195eff035b4168440e62a037be706bf1b039
Ivan <kiberaction@yandex.ru>
2016-10-13 22:06:53 +0300

    cloud found

--- src/main/java/com/router/config/MongoConfig.java
+++ src/main/java/com/router/config/MongoConfig.java
@@ -2,10 +2,13 @@ package com.router.config;
 
 import com.mongodb.*;
 import com.mongodb.client.MongoDatabase;
+import org.cloudfoundry.runtime.env.CloudEnvironment;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+import java.util.Map;
 
 /**
  * Created by asus-pc on 29.09.2016.
@@ -37,7 +40,16 @@ public class MongoConfig extends AbstractMongoConfiguration {
 
     @Override
     public Mongo mongo() throws Exception {
-        return new MongoClient(new MongoClientURI("mongodb://" + mongoDBLogin + ":" + mongoDBPassword + "@" + mongoHost + ":" + mongoPort + "/" + mongoDB));
+        return new MongoClient(getServiceURI());//new MongoClientURI("mongodb://" + mongoDBLogin + ":" + mongoDBPassword + "@" + mongoHost + ":" + mongoPort + "/" + mongoDB));
+    }
+
+    public String getServiceURI() throws Exception {
+        CloudEnvironment environment = new CloudEnvironment();
+        if ( environment.getServiceDataByLabels("mongodb-2.4").size() == 0 ) {
+            throw new Exception( "No MongoDB service is bound to this app!!" );
+        }
+        Map credential = (Map)((Map)environment.getServiceDataByLabels("mongodb-2.4").get(0)).get( "credentials" );
+        return (String)credential.get( "url" );
     }
 
 }
